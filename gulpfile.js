fbd9090d39e75bbeb2401e6ace76d0efff2e98b6
Steve Cochrane <steve@stevecochrane.com>
2016-11-06 17:55:20 -0600

    Removed Normalize.css since it's bundled with Tachyons.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,5 @@
 var gulp = require("gulp");
 
-//	Copy the unmodified Normalize CSS file from node_modules to public
-gulp.task("normalize", () => {
-	return gulp.src("node_modules/normalize.css/normalize.css")
-		.pipe(gulp.dest("public/css"));
-});
-
 //	Copy the unmodified, minified Tachyons CSS file from node_modules to public
 gulp.task("tachyons", () => {
 	return gulp.src("node_modules/tachyons/css/tachyons.min.css")
@@ -13,6 +7,5 @@ gulp.task("tachyons", () => {
 });
 
 gulp.task("default", [
-	"normalize",
-    "tachyons"
+	"tachyons"
 ]);
