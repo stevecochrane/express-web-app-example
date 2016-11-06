2ac490df785d1cba0258bf2b62f00d2acbdfd1ea
Steve Cochrane <steve@stevecochrane.com>
2016-11-06 15:20:48 -0600

    Added a Normalize.css dependency and another Gulp task that moves it to the public directory.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require("gulp");
 
+//	Copy the unmodified Normalize CSS file from node_modules to public
+gulp.task("normalize", () => {
+	return gulp.src("node_modules/normalize.css/normalize.css")
+		.pipe(gulp.dest("public/css"));
+});
+
 //	Copy the unmodified, minified Tachyons CSS file from node_modules to public
 gulp.task("tachyons", () => {
 	return gulp.src("node_modules/tachyons/css/tachyons.min.css")
@@ -7,5 +13,6 @@ gulp.task("tachyons", () => {
 });
 
 gulp.task("default", [
+	"normalize",
     "tachyons"
 ]);
