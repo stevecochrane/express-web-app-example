e1c1ef2266c08279c845f8fc072df34b70f7f4f2
Steve Cochrane <steve@stevecochrane.com>
2016-11-06 15:18:06 -0600

    Added Gulp, with just a simple 'tachyons' task that copies the Tachyons minified file to the public directory.

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require("gulp");
+
+//	Copy the unmodified, minified Tachyons CSS file from node_modules to public
+gulp.task("tachyons", () => {
+	return gulp.src("node_modules/tachyons/css/tachyons.min.css")
+		.pipe(gulp.dest("public/css"));
+});
+
+gulp.task("default", [
+    "tachyons"
+]);
